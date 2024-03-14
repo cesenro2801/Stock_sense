3232e6cbd6fa423b328bbc165d77c2c4aefb779f
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-03-13 23:58:45 -0500

    add sections in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
-import './App.css'
-import { MyRoutes } from "./index";
-import { AuthContextProvider } from './context/AuthContext';
+import styled, { ThemeProvider } from "styled-components";
+import { AuthContextProvider, MyRoutes, Light, Dark } from "./index";
+import { createContext, useState } from "react";
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const  [themeuse, setTheme] = useState('dark');
+  const theme = themeuse === "light" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
   return (
     <>
-     <AuthContextProvider>
-        <MyRoutes />
-      </AuthContextProvider>
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeProvider theme={themeStyle}>
+        <AuthContextProvider>
+          <Container>
+            <section className="ContentSidebar">
+
+            </section>
+            <section className="ContentMenuambur">
+              
+            </section>
+            <section className="ContentRoutes">
+              
+            </section>
+
+            <MyRoutes />
+          </Container>
+        </AuthContextProvider>
+        </ThemeProvider>
+      </ThemeContext.Provider>
     </>
-  )
+  );
 }
+
+const Container = styled.main`
+  display: grid; // es para que los elementos hijos se comporten como grid
+  grid-template-columns: 1fr; // es para que el grid tenga una sola columna
+  background-color: ${({theme})=>theme.bgtotal}; // es para que el fondo sea el color de fondo del tema
+`;
 
 export default App
--- src/styles/index.js
+++
@@ -1,3 +0,0 @@
-export { Device } from './breakpoints';
-export { Dark, Light } from './themes';
-export { v } from './variables';
