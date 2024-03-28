2c65b7dcb222c9ff5a93e7d992ac510c353a9f1b
Juan Guerrero Ortega <guerreroortegajuancamilo@gmail.com>
2024-03-28 00:14:30 -0500

    Protected routes

---
+++ src/components/templates/LoginTemplate.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+export function LoginTemplate() {
+    return (<Container>
+        <h1>Hola Login</h1>
+        </Container>);
+}
+const Container =styled.div`
+
+`
