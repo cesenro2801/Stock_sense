cef5be0f7311c9e82254de5c5c385e0676c44f20
Juan Guerrero Ortega <guerreroortegajuancamilo@gmail.com>
2024-03-27 23:09:30 -0500

    Insert users in the system

--- src/store/UsuariosStore.jsx
+++ src/store/UsuariosStore.jsx
@@ -1,10 +1,15 @@
 import { create } from "zustand";
+import { InsertarUsuarios } from "../index";
 
-export const useUsuariosStore = create ((set, get)=>({
-    insertarUsusarioAdmin: async (p)=> {
-        await supabase.auth.signUp({
+
+export const useUsuariosStore = create((set, get) => ({
+    insertarUsuarioAdmin: async (p) => {
+        const {data, error} = await SupabaseAuthClient.auth.signUp({
             email: p.correo,
             password: p.pass,
         });
-    }
-}))
+        console.log("data del registro del user",data)
+        if (error) return;
+        await InsertarUsuarios({idauth: data.user.id, fecharegistro: new Date(), tipouser:"admin"})
+    },
+}));
