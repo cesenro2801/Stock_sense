bf912574415a418af77a7bba06d4c9d29cb52709
cesenro2801 <cesar2191952@correo.uis.edu.co>
2024-04-09 23:34:25 -0500

    Uso de useQuery

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import { ErrorMolecula, Home, Login, ProtectedRoute, SpinnerLoader, UserAuth, useUsuariosStore } from "../index";
+import { ErrorMolecula, Home, Login, ProtectedRoute, SpinnerLoader, UserAuth, useEmpresaStore, useUsuariosStore } from "../index";
 import { useQuery } from "@tanstack/react-query";
 
 export const MyRoutes = () => {
   const {user} = UserAuth();
-  const {mostrarUsuarios}= useUsuariosStore();
-  const {data, isLoading, error} = useQuery({queryKey:["mostrar usuarios"], queryFn:mostrarUsuarios});
+  const {mostrarUsuarios, idusuario}= useUsuariosStore();
+  const {mostrarEmpresa}=useEmpresaStore();
+  const {data:datausuarios, isLoading, error} = useQuery({queryKey:["mostrar usuarios"], queryFn:mostrarUsuarios});
+  const {data:dataempresa}=useQuery({queryKey:["mostrar empresa"],queryFn:()=>mostrarEmpresa({idusuario:idusuario}),enabled:!!datausuarios})
   if(isLoading){
     return <SpinnerLoader/>
   }
---
+++ src/supabase/crudEmpresa.jsx
@@ -0,0 +1,12 @@
+import Swal from "sweetalert2";
+import {ObtenerIdAuthSupabase, supabase} from "../index"
+
+export const MostrarEmpresa = async (p) => {
+    const {error, data} = await supabase.from("asignarempresa").select(`empresa(id,nombre,simbolomoneda)`).eq("id_usuario", p.idusaurio)
+    .maybeSingle();
+    if (data){
+        return data;
+    }
+};
+
+
