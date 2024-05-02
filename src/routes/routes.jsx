5d3ac3030742aa6d386058782d95a569d92aceb1
Juan Camilo Guerrero Ortega <guerreroortegajuancamilo@gmail.com>
2024-05-01 23:43:45 -0500

    Module "Categorias"

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { ErrorMolecula, Home, Login, Configuracion, Marca, ProtectedRoute, SpinnerLoader, UserAuth, useEmpresaStore, useUsuariosStore } from "../index";
+import { ErrorMolecula, Home, Login, Configuracion, Marca, ProtectedRoute, SpinnerLoader, UserAuth, useEmpresaStore, useUsuariosStore, Categorias } from "../index";
 import { useQuery } from "@tanstack/react-query";
 
 export const MyRoutes = () => {
@@ -23,6 +23,7 @@ export const MyRoutes = () => {
               <Route path="/" element={<Home />} />
               <Route path="/configurar" element={<Configuracion />} />
               <Route path="/configurar/marca" element={<Marca />} />
+              <Route path="/configurar/categorias" element={<Categorias />} />
           </Route>
         </Routes>
   );
